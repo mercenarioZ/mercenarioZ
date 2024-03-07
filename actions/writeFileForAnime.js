2604d348acf2104e6414526923a4a83dc637343b
mercenarioz <funnystar320@gmail.com>
2024-03-07 09:27:56 +0700

    fix: markdown syntax

--- actions/writeFileForAnime.js
+++ actions/writeFileForAnime.js
@@ -4,6 +4,6 @@ export const writeFileForAnime = (data) => {
   fs.writeFileSync(
     "README.md",
 
-    `# Random anime every 8 hours\n\n**Title:** ${data.title}\n\n**English Title:** ${data.englishTitle}\n\n**Score:** ${data.score}\n\n**Type:** ${data.type}\n\n**Aired: ${data.startDate}\n\n**Genres: ${data.genres}\n\n**Episodes:** ${data.episodeCount}\n\n**Rating:** ${data.rating}\n\n**More information:** [${data.title}](${data.url})`
+    `# Random anime every 8 hours\n\n_**Title:**_ ${data.title}\n\n**English Title:** ${data.englishTitle}\n\n_**Score:**_ ${data.score}\n\n**Type:** ${data.type}\n\n**Aired:** ${data.startDate}\n\n**Genres:** ${data.genres}\n\n**Episodes:** ${data.episodeCount}\n\n**Rating:** ${data.rating}\n\n**More information:** [${data.title}](${data.url})`
   );
 };
